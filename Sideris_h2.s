e8780692e040f3ffa82381c12a62acef2ca270e4
PSid-R32 <55320989+PSid-R32@users.noreply.github.com>
2019-09-14 11:07:01 -0400

    Update Sideris_h2.s

--- Sideris_h2.s
+++ Sideris_h2.s
@@ -1,24 +1,4 @@
 #######################################################################################################################
-#Peter Sideris
-#May 5, 2019
-#Sideris_h2.s
-#DESCRIPTION
-#Program 2 asks the user to input 10 integer values, they were stored. The program then was to find the sum of the
-#values, find the maximum value, and then reverse the entries and display them in that order. I had trouble finding
-#the maximum value, as well as figuring out how to traverse the array in reverse order using the MIPS language.
-#PSUEDO-CODE
-#println("Enter 10 values: ");
-#for (i < array.length){
-#	myArray[i] = user_input.next
-#	println("These are tge values " + myArray[i]);
-#sum=0;
-#for(i < array.length){
-#sum += myArray[i]
-#}
-#int max = myArray[0]
-#for (i < myArray.length )
-#	if( mA[i] > max)
-#		max = mA[i]
 #REGISTER USE
 #$a0 : addressing 
 #$zero : zero register
